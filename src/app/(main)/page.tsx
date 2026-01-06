d87e5ccff016430f589d7095fc30e5c949b1af13
Adesiyan Tope <adesiyantope2014@gmail.com>
2026-01-06 07:26:25 +0100

    setup

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,8 +1,9 @@
+"use client";
+
 import Audience from "@/components/Audience";
 import Contact from "@/components/Contact";
 import Content from "@/components/Content";
 import FeatureSection from "@/components/Features";
-import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Testimonials from "@/components/Testimonials";
 import React from "react";
